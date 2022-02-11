38f819c261cf451d92fec9f018ef8de4f750ed1b
miller ossa samboni <miller.ossa@sofka.com.co>
2022-02-11 12:18:37 -0500

    edit date in program class

--- POOconcept/POOconcept/Program.cs
+++ POOconcept/POOconcept/Program.cs
@@ -4,7 +4,7 @@ using POOconcept;
 try
 {
     Console.WriteLine(new Date(2022, 12, 7));
-    Console.WriteLine(new Date(1974, 2, 7));
+    Console.WriteLine(new Date(1974, 2, 8));
     Console.WriteLine(new Date(1985, 11, 43));
 }
 catch (Exception error)
